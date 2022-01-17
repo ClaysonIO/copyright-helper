eecf1760ffe5546ac2daf69e8f2b0e7d076b177f
Daniel C <danclayson@gmail.com>
2022-01-17 16:22:23 -0500

    Make it pretty

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const path = require("path");
 const fs = require("fs");
 const ignore = require("ignore");
+const yargs = require('yargs');
+const {hideBin} = require("yargs/helpers")
+const readline = require('readline');
+const chalk = require('chalk');
+const prompt = require('prompt-sync')({
+  sigint: false
+});
 
-
-const rootDirectory = "D:\\GitHub\\copyright-test";
-const joinedFilePath = "D:\\GitHub\\copyright-helper\\joinedFile.txt";
-let copyrightEndStatement = "END OF COPYRIGHT NOTICE"
-
-
+//CONFIG VALUES
 //These are the types of files to which a copyright notice will be added, along with the start and end symbols for comments
 const supportedFileTypes = {
   ".js": ["//", ""],
@@ -16,22 +18,81 @@ const supportedFileTypes = {
   ".html": ["<!--", "-->"]
 }
 
-const copyrightNotice = Initialize(rootDirectory);
+try{
+//Introduction
+  console.log(chalk.blue.bold("Welcome to the Copyright Notice Helper"))
+  console.log(chalk.blue.bold("This tool adds a copyright notice to every supported file in a project."))
+  console.log(chalk.blue.bold("It will also output a single file with all supported filetypes combined, and a list of unsupported files."))
+  console.log();
+  console.log();
 
-fs.writeFileSync(joinedFilePath, "");
 
-consumeFolder(rootDirectory, joinedFilePath, "", [], copyrightNotice, copyrightEndStatement, supportedFileTypes);
+  const currentDirectory = __dirname;
+  const rootDirectory = prompt(chalk.greenBright(`Where is the project you would like to copyright? `) +  chalk.blueBright(`(${currentDirectory})`), currentDirectory);
+  console.log(rootDirectory)
+  const joinedFilePath = path.join(rootDirectory, ".output");
+  let copyrightEndStatement = "END OF COPYRIGHT NOTICE"
 
-function Initialize(rootDirectory){
-  //copyright notice and initial gitignore
-  const copyrightFile = fs.readFileSync(path.join(rootDirectory, ".copyright")).toString();
 
-  return copyrightFile;
+//Record and output a list of filetypes that were encountered, but not supported
+  const unsupportedFiles = {};
+  const supportedFiles = {};
+
+//Get the copyright notice
+  let copyrightNotice
+  try{
+    copyrightNotice = fs.readFileSync(path.join(rootDirectory, ".copyright")).toString();
+  } catch (e){
+    console.log(chalk.redBright("ERROR: Could not find .copyright file in root directory. Please create this file and try again."))
+    closeProgram();
+  }
+
+//Create an empty file for the joined output.
+  fs.writeFileSync(joinedFilePath, `Combined Output for ${rootDirectory}`);
+
+  consumeFolder(rootDirectory, joinedFilePath, "", [], copyrightNotice, copyrightEndStatement, supportedFileTypes, supportedFiles, unsupportedFiles);
+
+
+
+//OUTPUT AT END
+  console.log();
+  console.log(chalk.greenBright("Results:"))
+  console.log(chalk.greenBright("Supported Filetypes:"))
+  Object.keys(supportedFiles)
+    .forEach(key=>{
+      console.log(chalk.blue.bold(key.padEnd(10)), ": ", chalk.red(supportedFiles[key]))
+    });
+
+
+  console.log();
+  console.log(chalk.greenBright("Unsupported Filetypes:"))
+  Object.keys(unsupportedFiles)
+    .forEach(key=>{
+      console.log(chalk.blue.bold(key.padEnd(10)), ": ", chalk.red(unsupportedFiles[key]))
+    });
+
+  console.log();
+  console.log(chalk.greenBright("Joined files have been output to: "));
+  console.log(chalk.blueBright(joinedFilePath));
+
+  console.log();
+  console.log();
+
+  closeProgram();
+} catch(e){
+  console.log(chalk.red(e.stack))
+  closeProgram();
 }
 
 
+//SUPPORT FUNCTIONS
 
-function consumeFolder(rootDirectory, joinedFilePath, relativeFolderPath, gitignore, copyrightNotice, copyrightEndStatement, supportedFileTypes){
+function closeProgram(){
+  prompt("(Press any key to exit)")
+  process.exit();
+}
+
+function consumeFolder(rootDirectory, joinedFilePath, relativeFolderPath, gitignore, copyrightNotice, copyrightEndStatement, supportedFileTypes, supportedFiles, unsupportedFiles){
   try{
     const newIgnore = fs.readFileSync(path.join(rootDirectory, relativeFolderPath, '.gitignore')).toString().split('\r\n').filter(x=>x);
     gitignore = gitignore.concat(newIgnore);
@@ -44,7 +105,7 @@ function consumeFolder(rootDirectory, joinedFilePath, relativeFolderPath, gitign
   //Read all files and folders in a directory
   const filesAndFolders = fs
     .readdirSync(path.join(rootDirectory, relativeFolderPath), {withFileTypes: true})
-    .filter(x=>!ig.ignores(path.join(relativeFolderPath, x.name)) && x.name !== '.git')
+    .filter(x=>!ig.ignores(path.join(relativeFolderPath, x.name || "")) && x.name !== '.git')
 
 
   //Filter files and folders using gitignore
@@ -58,11 +119,19 @@ function consumeFolder(rootDirectory, joinedFilePath, relativeFolderPath, gitign
     .filter(x=>x.isDirectory())
     .map(x=>x.name);
 
-  console.log(files)
-
   //For each file, read contents into memory
   files
-    .filter(fileName=> Object.keys(supportedFileTypes).indexOf(path.extname(fileName)) > -1)
+    .filter(fileName=> {
+      //If file type isn't supported, log it.
+      const filetype = path.extname(fileName);
+      if(Object.keys(supportedFileTypes).indexOf(filetype) > -1){
+        supportedFiles[filetype] = (supportedFiles[filetype] || 0)  + 1;
+        return true;
+      } else {
+        unsupportedFiles[filetype] = (unsupportedFiles[filetype] || 0)  + 1;
+        return false;
+      }
+    })
     .forEach(fileName=>{
       const fileContents = fs.readFileSync(path.join(rootDirectory, relativeFolderPath, fileName));
       const fileType = path.extname(fileName);
@@ -71,12 +140,12 @@ function consumeFolder(rootDirectory, joinedFilePath, relativeFolderPath, gitign
       const scrubbedContents = scrubSensitiveData(contentsWithoutCopyright, fileType);
       const contentsWithNewCopyright = prependCopyrightNotice(contentsWithoutCopyright, copyrightNotice, copyrightEndStatement, fileType, supportedFileTypes)
 
-      appendFileToBatch(joinedFilePath, path.join(rootDirectory, relativeFolderPath, fileName), scrubbedContents);
+      appendFileToBatch(joinedFilePath, path.join(relativeFolderPath, fileName), scrubbedContents);
       saveCopyrightedFile(path.join(rootDirectory, relativeFolderPath, fileName), contentsWithNewCopyright)
     })
 
   folders.forEach(folderName=>{
-    consumeFolder(rootDirectory, joinedFilePath, path.join(relativeFolderPath, folderName), gitignore, copyrightNotice, copyrightEndStatement, supportedFileTypes)
+    consumeFolder(rootDirectory, joinedFilePath, path.join(relativeFolderPath, folderName), gitignore, copyrightNotice, copyrightEndStatement, supportedFileTypes, supportedFiles, unsupportedFiles)
   })
 }
 
@@ -89,6 +158,8 @@ function appendFileToBatch(joinedFilePath, fileNameWithPath, text){
   const allText = [
     "",
     "",
+    "",
+    "********************************************************",
     fileNameWithPath,
     "",
     text
